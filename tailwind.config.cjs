cdf7d6367e30aae2754f9ceebfcdc5513a7b8594
Stephen-Ibe <saintkalson@gmail.com>
2023-03-07 11:44:12 +0100

    setup css reset

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+            "3xl": "1600px",
+        },
         extend: {},
     },
     plugins: [],
