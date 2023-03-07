7b4336e6f929ef6b188331e3c293959688acb99e
Stephen-Ibe <saintkalson@gmail.com>
2023-03-07 14:14:36 +0100

    main section layout

---
+++ src/components/common/cards/NFTCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+const NFTCard = (props: Props) => <div>NFTCard</div>;
+
+export default NFTCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
 import React, { FC } from "react";
+import { NFTCard, SideNav } from "../components";
 
-const Home: FC = () => <div className="my-10">Home</div>;
+const Home: FC = () => (
+    <div className="my-10  flex">
+        <aside className="border w-3/12">
+            <SideNav />
+        </aside>
+        <main className="border w-9/12">
+            <NFTCard />
+        </main>
+    </div>
+);
 
 export default Home;
