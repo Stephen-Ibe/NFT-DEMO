2d0907e4537156061fcb7f2a9e381aad228425f9
Stephen-Ibe <saintkalson@gmail.com>
2023-03-07 11:50:14 +0100

    integrated UI Component Library

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
 import App from "./App";
 import "./assets/css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+            <App />
+        </MantineProvider>
     </React.StrictMode>
 );
