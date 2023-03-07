355180e7435a65b69cbd542d4d53d3e8dde18a2c
Stephen-Ibe <saintkalson@gmail.com>
2023-03-07 17:34:07 +0100

    card section

--- src/components/layout/WebLayout.tsx
+++ src/components/layout/WebLayout.tsx
@@ -5,7 +5,7 @@ import { Footer, Navbar } from "../blocks";
 const WebLayout = () => (
     <div className="m">
         <Navbar />
-        <section className="mt-24 sm:mt-0 px-4 lg:px-10">
+        <section className="mt-24 mb-20 sm:mt-0 px-4 lg:px-10">
             <Outlet />
         </section>
         <Footer />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,13 @@ import { NFTCard, NFTCardSkeleton, SideNav } from "../components";
 import { nft } from "../utils";
 
 const Home = () => (
-    <div className="flex items-start relative">
-        <aside className="border hidden sm:block w-3/12 p-4">
+    <div className="flex items-start relative container mx-auto">
+        <aside className="border hidden sm:block w-2/12 p-4">
             <SideNav />
         </aside>
-        <main className="w-full sm:w-9/12 p-4 overflow-y-scroll lg:h-[1235px]">
+        <main className="w-full sm:w-10/12 p-4 overflow-y-scroll h-[1000px] pb-44 ">
             <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                {[1, 2, 3, 4].map(() => (
+                {[1, 2, 3, 4, 2, 3, 4, 5, 1].map(() => (
                     <NFTCard nft={nft} />
                 ))}
                 <NFTCardSkeleton />
