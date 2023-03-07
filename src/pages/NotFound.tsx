c3587f8ad6c3f5baadfde4b331fb8d5b3afd9fa3
Stephen-Ibe <saintkalson@gmail.com>
2023-03-07 12:31:09 +0100

    404 Page + style

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFound = () => (
-    <div className="not_found">
-        <h1 className="text-8xl">404</h1>
+    <div className="not--found">
+        <h1>404</h1>
         <p className="tracking-widest	text-lg">- Page Not Found -</p>
-        <Link to="/" className="my-4">
-            <button type="button" className="py-2  border px-10">
+        <Link to="/" className="my-6">
+            <button type="button" className="btn">
                 Go back <strong>HOME</strong>
             </button>
         </Link>
