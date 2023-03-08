d70736f948d7ea65b77a597e12249594b4cc4acc
Stephen-Ibe <saintkalson@gmail.com>
2023-03-08 11:57:52 +0100

    updated Errorboundary

--- src/utils/hoc/ErrorBoundary.tsx
+++ src/utils/hoc/ErrorBoundary.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { useRouteError } from "react-router";
+import { Link } from "react-router-dom";
 
 const ErrorBoundary = () => {
     const error = useRouteError();
     console.error(error);
 
-    return <div>Error</div>;
+    return (
+        <div className="not--found">
+            <h1>Ooops...</h1>
+            <p className="tracking-widest	text-lg">- Something went wrong-</p>
+            <Link to="/" className="my-6">
+                <button type="button" className="btn">
+                    Go back <strong>HOME</strong>
+                </button>
+            </Link>
+        </div>
+    );
 };
 
 export default ErrorBoundary;
