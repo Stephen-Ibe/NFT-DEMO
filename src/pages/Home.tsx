044566c6cd0bac7d9939fdf330000de91f644d47
Stephen-Ibe <saintkalson@gmail.com>
2023-03-08 01:52:42 +0100

    NFT Details

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,9 +22,12 @@ const Home = () => {
                 <aside className="border hidden sm:block w-2/12 p-4">
                     <SideNav />
                 </aside>
-                <main className="w-full sm:w-10/12 p-4 overflow-y-scroll h-[1000px] pb-44 ">
+                <main className="w-full sm:w-10/12 p-4 overflow-y-scroll h-screen pb-44 ">
                     <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                        {[1, 2, 3, 4, 2, 3, 4, 5, 1].map(() => (
+                        {[
+                            1, 2, 3, 4, 2, 3, 2, 6, 7, 2, 1, 2, 3, 4, 5, 6, 7,
+                            8, 6, 4, 3, 4, 5, 1,
+                        ].map(() => (
                             <NFTCard nft={nft} showDetail={showDetail} />
                         ))}
                         <NFTCardSkeleton />
--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,5 +1,5 @@
 import React, { memo, useEffect, useState } from "react";
-import { Modal } from "@mantine/core";
+import { Modal, useMantineTheme } from "@mantine/core";
 import { NFTDataType } from "../types";
 
 type Props = {
@@ -10,14 +10,75 @@ type Props = {
 const ProductDetail = ({ close, nft }: Props) => {
     const [name, setName] = useState<string>("");
 
+    const theme = useMantineTheme();
+
     useEffect(() => {
         setName(nft.name);
         return () => setName("");
     });
 
     return (
-        <Modal opened onClose={close} title={name} size="lg">
-            Hello
+        <Modal
+            opened
+            onClose={close}
+            title={name}
+            size="55rem"
+            overlayProps={{
+                color:
+                    theme.colorScheme === "dark"
+                        ? theme.colors.dark[9]
+                        : theme.colors.gray[2],
+                opacity: 0.55,
+                blur: 3,
+            }}
+        >
+            <section className="flex h-full space-x-4">
+                <div className="img w-4/12 ">
+                    <div className="relative h-[250px] w-full">
+                        <img
+                            src={nft?.image_original_url}
+                            alt="nft__image"
+                            className="absolute w-full h-full object-cover rounded-t-lg"
+                        />
+                    </div>
+                    <div className="my-2 py-2">
+                        <h3 className="font-semibold text-base">{nft?.name}</h3>
+                        <p className="text-sm">
+                            Owned by{" "}
+                            <a
+                                href={nft?.collection?.external_url}
+                                className="text-blue-600"
+                            >
+                                {nft?.collection?.name}
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <div className="w-8/12 px-2">
+                    <p className="text-justify text-sm">
+                        {nft?.asset_contract?.description}
+                    </p>
+                    <div className="my-4">
+                        <h4 className="font-semibold text-lg">Traits</h4>
+                        <div className="grid grid-cols-3 gap-4 my-2">
+                            {nft?.traits.map(trait => (
+                                <div className="border text-center p-4 rounded-lg">
+                                    <p className="uppercase text-xs">
+                                        {trait.trait_type}
+                                    </p>
+                                    <h5 className="capitalize font-semibold">
+                                        {trait.value}
+                                    </h5>
+                                    <p className="text-xs">
+                                        {trait.trait_count} people have this
+                                        trait
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </section>
         </Modal>
     );
 };
