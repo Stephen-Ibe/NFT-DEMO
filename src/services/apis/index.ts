014c71b13a4ab8a77ee921e25f48d864d5f34437
Stephen-Ibe <saintkalson@gmail.com>
2023-03-08 02:52:43 +0100

    Axios Config adn APIs setup

---
+++ src/services/apis/index.ts
@@ -0,0 +1,24 @@
+import axios from "../axios";
+
+export const getAssetsApi = async () => {
+    try {
+        const url = "/assets";
+        const res = await axios.get(url);
+        return res;
+    } catch (err: unknown) {
+        throw err;
+    }
+};
+
+export const getSingleAssetApi = async (
+    assetContractAddress: string,
+    tokenId: string
+) => {
+    try {
+        const url = `/asset/${assetContractAddress}/tokenId`;
+        const res = await axios.get(url);
+        return res;
+    } catch (err: unknown) {
+        throw err;
+    }
+};
---
+++ src/services/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export { default as instance } from "./axios";
+export * from "./apis";
