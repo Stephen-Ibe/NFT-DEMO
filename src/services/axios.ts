f1e7079c30a1f92f5d639994eddfdf87c21df3de
Stephen-Ibe <saintkalson@gmail.com>
2023-03-08 02:44:51 +0100

    Axios Config

---
+++ src/services/axios.ts
@@ -0,0 +1,27 @@
+import axios, {
+    AxiosError,
+    InternalAxiosRequestConfig,
+    AxiosResponse,
+} from "axios";
+
+const instance = axios.create({
+    baseURL: "https://opensea13.p.rapidapi.com",
+    headers: {
+        "X-RapidAPI-Key": "", // b48ff04a7bmsh47c7f3264223a12p153687jsnaead187763f1
+        "X-RapidAPI-Host": "opensea13.p.rapidapi.com",
+        "Content-Type": "application/json",
+    },
+});
+
+instance.interceptors.request.use(
+    (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => config,
+    (error: AxiosError): Promise<AxiosError> => Promise.reject(error)
+);
+
+const successHandler = (response: AxiosResponse): AxiosResponse => response;
+const errorHandler = (error: AxiosError): Promise<AxiosError> =>
+    Promise.reject(error);
+
+instance.interceptors.response.use(successHandler, errorHandler);
+
+export default instance;
